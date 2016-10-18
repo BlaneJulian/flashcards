ef76754c220a307adb3c30020550fb81e68e0961
Andrea Faciane and Eric Lau <eric@>
2016-10-18 16:35:40 -0700

    mostly working flashcards

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("#js1").click(function(){
+    $("#js1").toggle();
+    $("#js2").toggle();
+  });
+  $("#js3").click(function(){
+    $("#js3").toggle();
+    $("#js4").toggle();
+  });
+  $("#js5").click(function(){
+    $("#js5").toggle();
+    $("#js6").toggle();
+  });
+  $("#js7").click(function(){
+    $("#js7").toggle();
+    $("#js8").toggle();
+  });
+
+});
